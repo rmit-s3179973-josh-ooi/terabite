6ee49c0cb89f135309ddcf4d9e9f8e24c48f9b91
Josh Ooi <josh.ronald.ooi@gmail.com>
2018-04-16 11:18:22 +1000

    debug benex error

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	@section('header')
 	<meta charset="UTF-8">
 	<title>Document</title>
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>	
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+	@show
+
 	@yield('css')
 	{!! Html::script(asset('assets/js/jquery.min.js')) !!}	
-	@yield('header')
+	@yield('js')
 </head>
 <body>
 	<div class="container">		
