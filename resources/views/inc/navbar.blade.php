97c53af92e2138ca6e3e6ebf2cfb7feb44804457
s3540147 <37095246+s3540147@users.noreply.github.com>
2018-04-23 14:45:29 +1000

    Update homepage navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,32 +1,81 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Terabite</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/login">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">profile</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+<nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#">
+      <img src="" width="30" height="30" class="d-inline-block align-top" alt="">Terabite
+    </a>
+    <div class="d-flex ml-auto">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#globalNavbar" aria-controls="globalNavbar" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+    </div>
+    <div class="collapse navbar-collapse" id="globalNavbar">
+      <ul class="navbar-nav mr-auto order-1">
+        <form class="form-inline form-navbar my-2 my-lg-0 order-2" action="https://themes.getbootstrap.com/shop/">
+        <input class="form-control" name="s" type="text" placeholder="Search"/>
         </form>
-      </div>
-    </nav>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <div class="navbar-collapse navbar-top-collapse">
+              <ul id="menu-top-menu" class="nav navbar-nav">
+                <li id="menu-item-601" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-601">
+                  <a title="Admin &amp; Dashboard" href="https://themes.getbootstrap.com/product-category/admin-dashboard/">Admin &#038; Dashboard</a>
+                </li>
+                <li id="menu-item-603" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-603">
+                  <a title="Landing &amp; Corporate" href="https://themes.getbootstrap.com/product-category/landing-corporate/">Landing &#038; Corporate</a>
+                </li>
+                <li id="menu-item-1537" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1537">
+                  <a title="Application" href="https://themes.getbootstrap.com/product-category/application/">Application</a>
+                </li>
+                <li id="menu-item-602" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-602">
+                  <a title="E-Commerce &amp; Retail" href="https://themes.getbootstrap.com/product-category/ecommerce-retail/">E-Commerce &#038; Retail</a>
+                </li>
+                <li id="menu-item-1538" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1538">
+                  <a title="Portfolio &amp; Blog" href="https://themes.getbootstrap.com/product-category/portfolio-blog/">Portfolio &#038; Blog</a>
+                </li>
+                <li id="menu-item-1539" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1539">
+                  <a title="Specialty" href="https://themes.getbootstrap.com/product-category/specialty/">Specialty</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <ul class="navbar-nav d-none d-lg-flex ml-2 order-3">
+        <li class="btn btn-cart pull-right">
+          <button type="button" class="btn btn-outline-primary">cart</button>
+        </li>
+      </ul>
+      <ul class="navbar-nav d-lg-none">
+        <li class="nav-item-divider"></li>
+        <li class="btn btn-cart pull-right">
+          <button type="button" class="btn btn-outline-primary">cart</button>
+        </li>
+      </ul>
+    </div>
+</nav>
+<div class="navbar navbar-expand-lg navbar-light p-3 mb-2 bg-primary text-white">
+  <ul class="navbar-nav mr-auto order-1">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Categories</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Brands</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">About Us</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Contact Us</a>
+    </li>
+  </ul>
+  <ul class="navbar-nav d-none d-lg-flex ml-2 order-3">
+    <li class="nav-item"><a class="nav-link" href="https://themes.getbootstrap.com/signin/">Sign in</a>
+    </li>
+    <li class="nav-item"><a class="nav-link" href="https://themes.getbootstrap.com/my-account/">Sign up</a>
+    </li>
+  </ul>
+  <ul class="navbar-nav d-lg-none">
+    <li class="nav-item-divider"></li>
+    <li class="nav-item"><a class="nav-link" href="https://themes.getbootstrap.com/signin/">Sign in</a></li>
+    <li class="nav-item"><a class="nav-link" href="https://themes.getbootstrap.com/my-account/">Sign up</a></li>
+  </ul>
+</div>
