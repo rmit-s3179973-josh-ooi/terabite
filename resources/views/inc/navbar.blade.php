dc9af2a288370e7e7b93f46e3cdf853c3365b81b
s3540147 <37095246+s3540147@users.noreply.github.com>
2018-04-24 16:40:44 +1000

    updated css design for footer, navbar and the item boxes. Links changed colors to white to be able to be read sign up links connected to login and register page added items boxes with non-functional links to view and add to cart.

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,58 +1,67 @@
 
-<nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="#">
+@section('css')
+  {!! Html::style(asset('css/website/form.css')) !!}
+@endsection
+@section('content')
+
+<nav id="link1" class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="">
       <img src="" width="30" height="30" class="d-inline-block align-top" alt="">Terabite
     </a>
-    <div class="d-flex ml-auto">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#globalNavbar" aria-controls="globalNavbar" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+    <div class="col-lg-6">
+      <div class="input-group">
+        <div>
+          <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+        </div>
+        <input type="text" class="form-control rounded-left" placeholder="Search">
+        <div class="input-group-btn">
+          <select aria-label="Categories" class="nav-link dropdown-toggle" size="1" id="navbarDropdownMenuLink">
+            <option value="0">All Categories</option>
+            <option value="">Computers &amp; Laptops</option>
+            <option value="">Headphones</option>
+            <option value="">Printers</option>
+            <option value="">Speakers</option>
+            <option value="">Tvs</option>
+            <option value="">Phones</option>
+            <option value="">Cameras</option>
+            <option value="">Accessories</option>
+            <option value="">Hardware</option>
+            <option value="">Coins</option>
+            <option value="">Collectables</option>
+          </select>
+        </div>
+      </div>
     </div>
-    <div class="collapse navbar-collapse" id="globalNavbar">
-      <ul class="navbar-nav mr-auto order-1">
-        <form class="form-inline form-navbar my-2 my-lg-0 order-2" action="https://themes.getbootstrap.com/shop/">
-        <input class="form-control" name="s" type="text" placeholder="Search"/>
-        </form>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <div class="navbar-collapse navbar-top-collapse">
-              <ul id="menu-top-menu" class="nav navbar-nav">
-                <li id="menu-item-601" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-601">
-                  <a title="Admin &amp; Dashboard" href="https://themes.getbootstrap.com/product-category/admin-dashboard/">Admin &#038; Dashboard</a>
-                </li>
-                <li id="menu-item-603" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-603">
-                  <a title="Landing &amp; Corporate" href="https://themes.getbootstrap.com/product-category/landing-corporate/">Landing &#038; Corporate</a>
-                </li>
-                <li id="menu-item-1537" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1537">
-                  <a title="Application" href="https://themes.getbootstrap.com/product-category/application/">Application</a>
-                </li>
-                <li id="menu-item-602" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-602">
-                  <a title="E-Commerce &amp; Retail" href="https://themes.getbootstrap.com/product-category/ecommerce-retail/">E-Commerce &#038; Retail</a>
-                </li>
-                <li id="menu-item-1538" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1538">
-                  <a title="Portfolio &amp; Blog" href="https://themes.getbootstrap.com/product-category/portfolio-blog/">Portfolio &#038; Blog</a>
-                </li>
-                <li id="menu-item-1539" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1539">
-                  <a title="Specialty" href="https://themes.getbootstrap.com/product-category/specialty/">Specialty</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </li>
-      </ul>
+
+    <div class="d-flex ml-auto">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#globalNavbar" aria-controls="globalNavbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+
+    <div>
       <ul class="navbar-nav d-none d-lg-flex ml-2 order-3">
         <li class="btn btn-cart pull-right">
-          <button type="button" class="btn btn-outline-primary">cart</button>
+          <button type="button" class="btn btn-outline-primary">
+            Cart <span class="badge badge-light">4</span>
+          </button>
         </li>
       </ul>
       <ul class="navbar-nav d-lg-none">
         <li class="nav-item-divider"></li>
         <li class="btn btn-cart pull-right">
-          <button type="button" class="btn btn-outline-primary">cart</button>
+          <button type="button" class="btn btn-outline-light text-white">
+            Cart <span class="badge badge-light">4</span>
+          </button>
         </li>
       </ul>
+
+
     </div>
 </nav>
-<div class="navbar navbar-expand-lg navbar-light p-3 mb-2 bg-primary text-white">
+
+<div id="link2" class="navbar navbar-expand-lg navbar-light ">
   <ul class="navbar-nav mr-auto order-1">
     <li class="nav-item">
       <a class="nav-link" href="#">Categories</a>
@@ -68,14 +77,15 @@
     </li>
   </ul>
   <ul class="navbar-nav d-none d-lg-flex ml-2 order-3">
-    <li class="nav-item"><a class="nav-link" href="https://themes.getbootstrap.com/signin/">Sign in</a>
-    </li>
-    <li class="nav-item"><a class="nav-link" href="https://themes.getbootstrap.com/my-account/">Sign up</a>
-    </li>
+    <li class="nav-item"><a class="nav-link" href="login">Login</a></li>
+    <li class="nav-item"><a class="nav-link" href="register">Sign up</a></li>
   </ul>
   <ul class="navbar-nav d-lg-none">
     <li class="nav-item-divider"></li>
-    <li class="nav-item"><a class="nav-link" href="https://themes.getbootstrap.com/signin/">Sign in</a></li>
-    <li class="nav-item"><a class="nav-link" href="https://themes.getbootstrap.com/my-account/">Sign up</a></li>
+    <li class="nav-item"><a class="nav-link" href="login">Login</a></li>
+    <li class="nav-item"><a class="nav-link" href="register">Sign up</a></li>
   </ul>
 </div>
+
+
+
