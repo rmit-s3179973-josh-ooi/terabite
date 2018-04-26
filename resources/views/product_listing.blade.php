a15f9cd5a536c0f4defdb2bf9b42a41c7da6086d
Casey Yan“ <s3679535@student.rmit.edu.au>
2018-04-27 00:16:04 +1000

    h

--- resources/views/product_listing.blade.php
+++ resources/views/product_listing.blade.php
@@ -2,8 +2,32 @@
 
 @section('content')
 @foreach ($products as $product)
-<div><strong>{{$product->product_name}}</strong></div>
-{{$product->product_description}}
-{{$product->price}}
+
+
+
+ <div class="row">
+            <div class="col-md-12">
+                <table class="table table-striped">
+                    <thead>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>Description</td>
+                    <td>Price</td>
+                    <td></td>
+                    </thead>
+                    <tbody>
+                    @foreach ($products as $product)
+                        <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->product_name}}</td>
+                    <td>{{$product->product_description}}</td>
+                    <td>{{$product->price}}</td> </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
 @endforeach
 @endsection
