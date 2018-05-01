7fd5e976d1af4d0c15f75fbff67e00a986a523fa
yestin <s3568499@student.rmit.edu.au>
2018-05-01 16:37:09 +1000

    update

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,14 +6,17 @@
 @endsection
 
 @section('js')
+{!! Html::script(asset('/assets/js/quantity.js')) !!}
 
 @endsection
 
 
 @section('content')
 
-{{ dd($product)}}
-	{{$product->product_category_id}}
+
+	{{$product->category->category_name}}
+	<br/>
+	<br/>
 <h2><strong>{{$product->product_name}}</strong></h2>
 
 	<div class="product-image" name ="img" >
@@ -23,47 +26,59 @@
 	</div>
 	 
 
+<div class="box">
 
-<div class="product-price">
-         <a class="qty"> QTY: <input type="text" class="qty" name="qty" value="1"></a>
-		<span class="separator">|</span>
-            <a>You pay: $ {{ $product->price }}</a>
-        </div>
+         
 		
-<div class="product-d">
-      <p>Devlivery:<a>$9.99  <span class="separator">|</span> Rate: </a></p>
-     </div>
-
-<div class="product-but">
-<form>
-               
-                	
-                <button class="btn btn-second" type="submit">Buy now</button>
-                <button class="btn btn-primary" type="submit">Add to Cart</button>
-            </form>
-	</div>
-	<br/>
-	<br/>
-
-	<div class="nav">
-	<ul>
-	<li class="active"><a href="#1" data-toggle="tab">Overview</a></li>
-	<li><a href="#2" data-toggle="tab">Reviews</a></li>
-	<li><a href="#3" data-toggle="tab">Q&A</a></li>
-	<li><a href="#4" data-toggle="tab">Specs</a></li>
-	</ul>
-     <br/>
-     </div>
-	 
-	<div id="1" class="tab-pane">
-	<br/>
-  <br/>
-  <br/>
-  <br/>
-<p>{{$product->product_description}}</p>
-
-
+            <p class="product-price">AU: $ {{ $product->price }}</p>
+        
+		
+		<h6>Delivery Option:</h6>
+		<h5>Standard Delivery</h5>
+		<h4>Free</h4>
+		<p class="time">Get By: 17 Apr - 20 Apr</p>
+		<h6 class="qua">Quantity</h6>
+		
+	<div class = "quantity">
+  <input type="number" min="1" max="50" step="1" value="1">
 </div>
+   
+
+
+<div class="product-but">        	
+                
+        <button class="btn btn-primary" type="submit">Add to Cart</button>
+            
+	</div>
+	</div>
+	
+	
+	<br/>
+	
+	<h4 class="des">Product Description</h4>
+	<br/>
+
+    <p>{{$product->product_description}}</p>
+
+  <br/>
+ 
+  <h4 class="rev">Product Review</h4>
+<br/>
+
+<div class="r1">
+<p class="name">John Doe</p><p class="date"> Nov 13 18</p>
+The width of this div remains at 300px, 
+in spite of the 50px of total left and right padding, 
+because of the box-sizing: border-box property.
+</div>
+<br/>
+<div class="r2">
+<p class="name2">John Doe</p><p class="date2"> Nov 13 18</p>
+The width of this div remains at 300px, 
+in spite of the 50px of total left and right padding, 
+because of the box-sizing: border-box property.
+</div>
+
 
 
 
